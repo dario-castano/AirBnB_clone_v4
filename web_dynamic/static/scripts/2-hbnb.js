ce24f3d9eb77ab6c9149ae5b20fcbcd62a1f7f95
Dario Castaño <dario.castano@aim.com>
2020-02-16 10:26:07 -0500

    Add forgotten code on 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,18 @@
+const checkedAmenities = {};
 $(document).ready(function () {
+  $('input:checkbox').on('click', function () {
+    const { id, name } = $(this).data();
+    if (this.checked) {
+      checkedAmenities[id] = name;
+    } else {
+      delete checkedAmenities[id];
+    }
+
+    const selectedAmenities = Object.values(checkedAmenities).join(', ');
+
+    $('.amenities h4').text(selectedAmenities);
+  });
+
   $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
     if (data.status === "OK") $('#api_status').addClass('available');
   });
